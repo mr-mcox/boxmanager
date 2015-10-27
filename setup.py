39818402f2550ab0106b82ba9b54c003bbec4142
Matthew Cox <matthew.cox@teachforamerica.org>
2015-10-27 16:19:05 -0500

    Try 3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='boxmanager',
       version='0.1',
-      py_modules=[''],
+      py_modules=['authenticate', 'box_wrapper', 'cli'],
       install_requires=['boxsdk>=1.2.2',
                         'py>=1.4.30',
                         'pytest>=2.8.2',

3b8251d2b177b615a9606e755219202d00df0186
Matthew Cox <matthew.cox@teachforamerica.org>
2015-10-27 16:28:31 -0500

    Switching to setuptools

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 setup(name='boxmanager',
       version='0.1',
-      py_modules=['authenticate', 'box_wrapper', 'cli'],
+      packages=find_packages(exclude=['docs', 'tests']),
       install_requires=['boxsdk>=1.2.2',
                         'py>=1.4.30',
                         'pytest>=2.8.2',
                         'PyYAML>=3.11',
                         'requests>=2.8.1',
                         'six>=1.10.0',
-                        ]
+                        ],
+      author="Matthew Cox",
+      author_email="matthew.cox@teachforamerica.org",
       )

780719dc4d9144d0188a66b03bf71719766f6360
Matthew Cox <matthew.cox@teachforamerica.org>
2015-10-27 15:43:08 -0500

    Adding setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+setup(name='boxmanager',
+      version='1.0',
+      py_modules=['authenticate', 'box_wrapper', 'cli'],
+      install_requires=['boxsdk>=1.2.2',
+                        'py>=1.4.30',
+                        'pytest>=2.8.2',
+                        'PyYAML>=3.11',
+                        'requests>=2.8.1',
+                        'six>=1.10.0',
+                        ]
+      )

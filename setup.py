f69e363c464760771857bbf48dea31ab5583accc
Matthew Cox <matthew.cox@teachforamerica.org>
2015-10-27 15:53:45 -0500

    Changing setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 setup(name='boxmanager',
       version='0.1',
-      py_modules=['authenticate', 'box_wrapper', 'cli'],
+      packages=find_packages(exclude=['docs', 'tests']),
       install_requires=['boxsdk>=1.2.2',
                         'py>=1.4.30',
                         'pytest>=2.8.2',
